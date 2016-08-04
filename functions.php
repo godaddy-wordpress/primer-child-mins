db5264ef6c49e2400b95b4305031ef9af1f844df
Chris Wallace <chris@liftux.com>
2016-08-04 00:55:07 -0500

    Fix page layout width and header image/hero layout.

--- functions.php
+++ functions.php
@@ -1,6 +1,57 @@
 <?php
 
 /**
+ * Move titles above menu templates.
+ *
+ * @since 1.0.0
+ */
+function mins_remove_titles(){
+
+	remove_action( 'primer_after_header', 'primer_add_page_builder_template_title', 100 );
+	remove_action( 'primer_after_header', 'primer_add_blog_title', 100 );
+	remove_action( 'primer_after_header', 'primer_add_archive_title', 100 );
+
+	if( ! is_front_page() ):
+		add_action( 'primer_after_header', 'primer_add_page_builder_template_title' );
+		add_action( 'primer_after_header', 'primer_add_blog_title' );
+		add_action( 'primer_after_header', 'primer_add_archive_title' );
+	endif;
+
+}
+add_action( 'init', 'mins_remove_titles' );
+
+/**
+ * Check to see if we should add the hero to the page.
+ *
+ * @action after_setup_theme
+ * @since 1.0.0
+ */
+function mins_check_hero() {
+
+	remove_action( 'primer_header', 'primer_add_hero', 10 );
+
+	if ( is_404() || is_page_template( 'templates/page-builder-no-header.php' ) ) {
+		return;
+	}
+
+	add_action( 'primer_after_header', 'mins_add_hero', 10 );
+
+}
+add_action( 'template_redirect', 'mins_check_hero' );
+
+/**
+ * Display hero in the header on the front page.
+ *
+ * @action primer_after_header
+ */
+function mins_add_hero(){
+	if( is_front_page() ) {
+		get_template_part( 'templates/parts/hero' );
+	}
+}
+add_action( 'primer_after_header', 'mins_add_hero', 30 );
+
+/**
  * Register custom Custom Navigation Menus.
  *
  * @link https://codex.wordpress.org/Function_Reference/register_nav_menus
@@ -87,7 +138,7 @@ add_action( 'widgets_init', 'mins_register_hero_sidebar' );
 function mins_get_header_image() {
 	$image_size = (int) get_theme_mod( 'full_width' ) === 1 ? 'hero-2x' : 'hero';
 	$custom_header = get_custom_header();
-	
+
 	if ( ! empty( $custom_header->attachment_id ) ) {
 		$image = wp_get_attachment_image_url( $custom_header->attachment_id, $image_size );
 		if ( getimagesize( $image ) ) {
@@ -161,12 +212,12 @@ function mins_update_font_types() {
 }
 add_action( 'primer_font_types', 'mins_update_font_types' );
 
-function escapade_font_weight( $weights, $font ) {
+function mins_font_weight( $weights, $font ) {
 
     return ( 'Roboto' === $font ) ? [ 700, 300, 100 ] : $weights;
 
 }
-add_filter( 'primer_font_weights', 'escapade_font_weight', 10, 2 );
+add_filter( 'primer_font_weights', 'mins_font_weight', 10, 2 );
 
 /**
  * Add Social links to primary navigation area.
@@ -186,7 +237,7 @@ add_action( 'primer_after_header', 'mins_add_social_to_header', 30 );
 
 /**
  * Remove customizer features added by the parent theme that are not applicable to this theme
- * 
+ *
  * @action after_setup_theme
  */
 function mins_remove_customizer_features($wp_customize){
@@ -198,7 +249,7 @@ add_action( 'customize_register', 'mins_remove_customizer_features', 30 );
 
 /**
  * Update colors
- * 
+ *
  * @action primer_colors
  */
 function mins_colors() {
@@ -345,7 +396,7 @@ function mins_add_default_header_image($array) {
 	$array['width'] = 1300;
 	$array['height'] =1245;
 	$array['flex-height'] = false;
-	
+
 	return $array;
 }
 add_filter( 'primer_custom_header_args', 'mins_add_default_header_image', 20 );
@@ -353,7 +404,7 @@ add_filter( 'primer_custom_header_args', 'mins_add_default_header_image', 20 );
 
 /**
  * Move navigation
- * 
+ *
  * @action primer_after_header
  */
 function mins_move_nav_markup() {
@@ -361,21 +412,9 @@ function mins_move_nav_markup() {
 	remove_action( 'primer_after_header', 'primer_add_primary_navigation', 20);
 
 	add_action( 'primer_header', 'primer_add_primary_navigation', 21);
-	 
+
 }
 add_action( 'init', 'mins_move_nav_markup', 30 );
-
-/**
- * Display hero in the header
- *
- * @action primer_after_header
- */
-function mins_add_hero(){
-	if( is_front_page() ) {
-		get_template_part( 'templates/parts/hero' );
-	}
-}
-add_action( 'primer_after_header', 'mins_add_hero', 30 );
 
 function mins_add_search_menu ( $items, $args ) {
 
