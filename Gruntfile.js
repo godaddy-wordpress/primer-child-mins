5b23043c3a34451dae32f9eada5ac8113de297fb
Chris Wallace <chris@liftux.com>
2016-08-11 14:21:53 -0500

    Escape string in html output

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+/* global module, require */
+
 module.exports = function(grunt) {
 
 	var pkg = grunt.file.readJSON( 'package.json' );
@@ -8,10 +10,19 @@ module.exports = function(grunt) {
 
 		autoprefixer: {
 			options: {
-				// Task-specific options go here.
+				browsers: [
+					'Android >= 2.1',
+					'Chrome >= 21',
+					'Edge >= 12',
+					'Explorer >= 7',
+					'Firefox >= 17',
+					'Opera >= 12.1',
+					'Safari >= 6.0'
+				],
+				cascade: false
 			},
-			your_target: {
-				src: '*.css'
+			dist: {
+				src: [ '*.css', 'assets/css/**/*.css' ]
 			}
 		},
 
@@ -24,13 +35,64 @@ module.exports = function(grunt) {
 					{
 						src: 'style.css',
 						dest: 'style-rtl.css'
-					}
+					},
+					{
+						src: 'editor-style.css',
+						dest: 'editor-style-rtl.css'
+					}/*,
+					{
+						expand: true,
+						cwd: 'assets/css/admin',
+						src: [ '*.css','!*-rtl.css','!*.min.css','!*-rtl.min.css' ],
+						dest: 'assets/css/admin',
+						ext: '-rtl.css'
+					}*/
 				]
 			}
 		},
 
+		cssmin: {
+			options: {
+				shorthandCompacting: false,
+				roundingPrecision: 5,
+				processImport: false
+			},
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'assets/css',
+					src: [ '*.css', '!*.min.css' ],
+					dest: 'assets/css',
+					ext: '.min.css'
+				}/*,
+				{
+					expand: true,
+					cwd: 'assets/css/admin',
+					src: [ '*.css', '!*.min.css' ],
+					dest: 'assets/css/admin',
+					ext: '.min.css'
+				}*/]
+			}
+		},
+
+		devUpdate: {
+			main: {
+				options: {
+					updateType: 'force', //just report outdated packages
+					reportUpdated: false, //don't report up-to-date packages
+					semver: true, //stay within semver when updating
+					packages: {
+						devDependencies: true, //only check for devDependencies
+						dependencies: false
+					},
+					packageJson: null, //use matchdep default findup to locate package.json
+					reportOnlyPkgs: [] //use updateType action on all packages
+				}
+			}
+		},
+
 		jshint: {
-			all: ['Gruntfile.js', 'assets/js/*.js', '!assets/js/*.min.js']
+			all: [ 'Gruntfile.js', 'assets/js/**/*.js', '!assets/js/**/*.min.js' ]
 		},
 
 		po2mo: {
@@ -69,20 +131,28 @@ module.exports = function(grunt) {
 				msgmerge: true
 			},
 			files:{
-				src: [
-					'*.php',
-					'inc/**/*.php',
-					'templates/**/*.php'
-				],
+				src: [ '**/*.php' ],
 				expand: true
 			}
 		},
 
 		replace: {
 			pot: {
-				src: 'languages/' + pkg.name + '.pot',
+				src: 'languages/*.po*',
 				overwrite: true,
 				replacements: [
+					{
+						from: 'SOME DESCRIPTIVE TITLE.',
+						to: pkg.title
+					},
+					{
+						from: 'YEAR THE PACKAGE\'S COPYRIGHT HOLDER',
+						to: new Date().getFullYear()
+					},
+					{
+						from: 'FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.',
+						to: 'GoDaddy Operating Company, LLC.'
+					},
 					{
 						from: 'charset=CHARSET',
 						to: 'charset=UTF-8'
@@ -93,69 +163,69 @@ module.exports = function(grunt) {
 
 		sass: {
 			options: {
+				precision: 5,
 				sourceMap: false
 			},
 			dist: {
-				files: {
-					'style.css': '.dev/sass/style.scss'
-				}
+				options: {
+					require: 'susy'
+				},
+				files: [
+					{
+						'style.css': '.dev/sass/style.scss',
+						'editor-style.css': '.dev/sass/editor-style.scss'
+					}/*,
+					{
+						expand: true,
+						cwd: '.dev/sass/admin',
+						src: ['*.scss'],
+						dest: 'assets/css/admin',
+						ext: '.css'
+					}*/
+				]
 			}
 		},
-
+/*
 		uglify: {
 			options: {
 				ASCIIOnly: true
 			},
-			core: {
+			dist: {
 				expand: true,
 				cwd: 'assets/js',
 				src: ['*.js', '!*.min.js'],
 				dest: 'assets/js',
 				ext: '.min.js'
+			},
+			admin: {
+				expand: true,
+				cwd: 'assets/js/admin',
+				src: ['*.js', '!*.min.js'],
+				dest: 'assets/js/admin',
+				ext: '.min.js'
 			}
 		},
-
-		devUpdate: {
-			main: {
-				options: {
-					updateType: 'force', //just report outdated packages
-					reportUpdated: false, //don't report up-to-date packages
-					semver: true, //stay within semver when updating
-					packages: {
-						devDependencies: true, //only check for devDependencies
-						dependencies: false
-					},
-					packageJson: null, //use matchdep default findup to locate package.json
-					reportOnlyPkgs: [] //use updateType action on all packages
-				}
-			}
-	    },
-
-
+*/
 		watch: {
 			css: {
 				files: '.dev/sass/**/*.scss',
-				tasks: ['sass','autoprefixer','cssjanus']
+				tasks: [ 'sass','autoprefixer','cssjanus' ]
 			},
 			scripts: {
-				files: ['Gruntfile.js', 'assets/js/*.js', '!assets/js/*.min.js'],
-				tasks: ['jshint', 'uglify'],
+				files: [ 'Gruntfile.js', 'assets/js/**/*.js', '!assets/js/**/*.min.js' ],
+				tasks: [ 'jshint'/*, 'uglify'*/ ],
 				options: {
 					interrupt: true
 				}
-			},
-			pot: {
-				files: [ '**/*.php' ],
-				tasks: ['update-pot']
-			},
+			}
 		}
 
 	});
 
 	require('matchdep').filterDev('grunt-*').forEach( grunt.loadNpmTasks );
 
-	grunt.registerTask('default', ['watch']);
-	grunt.registerTask('lint', ['jshint']);
-	grunt.registerTask('update-pot', ['pot', 'replace:pot']);
+	grunt.registerTask( 'default', [ 'sass', 'autoprefixer', 'cssjanus', 'cssmin', 'jshint'/*, 'uglify'*/ ] );
+	grunt.registerTask( 'lint', [ 'jshint' ] );
+	grunt.registerTask( 'update-pot', [ 'pot', 'replace:pot' ] );
 
 };
--- templates/parts/site-info.php
+++ templates/parts/site-info.php
@@ -24,7 +24,7 @@
 
 				?>
 
-				<?php printf( _x( 'Built with %1$s by %2$s.', '1. theme name link, 2. theme author link', 'primer' ), '<a href="https://wordpress.org/themes/primer/" rel="designer">Primer</a>', '<a href="https://www.godaddy.com/" rel="designer">GoDaddy</a>' ) ?>
+				<?php printf( esc_html_x( 'Built with %1$s by %2$s.', '1. theme name link, 2. theme author link', 'primer' ), '<a href="https://wordpress.org/themes/primer/" rel="designer">Primer</a>', '<a href="https://www.godaddy.com/" rel="designer">GoDaddy</a>' ) ?>
 
 			</div><!-- .site-info-text -->
 
